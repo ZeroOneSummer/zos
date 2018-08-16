c26ad7c30af01ceb1f194078a92bd0d9fcfc3b1a
ZeroOneSummer <592706328@qq.com>
2018-08-16 23:26:28 +0800

    文件导入导出

--- src/main/java/com/dm/zos/controller/orderSysReport/controller/OrderSysReportController.java
+++ src/main/java/com/dm/zos/controller/orderSysReport/controller/OrderSysReportController.java
@@ -1,7 +1,8 @@
 package com.dm.zos.controller.orderSysReport.controller;
 
 import cn.hutool.core.io.FileUtil;
-import com.alibaba.fastjson.JSONObject;
+import cn.hutool.core.io.IoUtil;
+import cn.hutool.json.JSONUtil;
 import com.dm.zos.controller.orderSysReport.entity.BBill;
 import com.dm.zos.controller.orderSysReport.service.OrderSysReportService;
 import com.github.pagehelper.PageHelper;
@@ -17,6 +18,11 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URLEncoder;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -70,5 +76,94 @@ public class OrderSysReportController {
 		logger.info("orderSysReport-download文件全部写入完成，文件路径:{}", filePath);
 	}
 
+	/*
+	* 读取文件信息到list里
+	* */
+	@RequestMapping("/import")
+	public void importData() throws Exception {
+		File file = new File("D:/20171201_20180430订单信息查询.txt");
+		InputStream is = new FileInputStream(file);
+		List list = new ArrayList();
+		IoUtil.readUtf8Lines(is,list);
+		List<BBill> list2 = new ArrayList<>();
+
+		list.stream()
+				.forEach(bill -> {
+					BBill bBill = new BBill();
+					bBill = JSONUtil.toBean((String) bill,BBill.class);
+					list2.add(bBill);
+				});
+	}
+
+
+	/*
+	*下载文件方式1
+	**/
+	@RequestMapping("/downFile")
+	public void downFile(HttpServletResponse response) throws Exception {
+		File file = new File("D:/20171201_20180430订单信息查询.txt");
+		InputStream is = null;
+		OutputStream os = null;
+		is = new FileInputStream(file);
+		int num = 0;
+		byte[] buff = new byte[1024];
+		os = response.getOutputStream();
+		response.setContentType("application/x-download");
+		response.setHeader("Content-Disposition","attachment;filename="+URLEncoder.encode("我是谁.txt","utf-8"));
+		while ((num=is.read(buff)) != -1){
+			os.write(buff,0,num);
+		}
+		os.close();
+		is.close();
+	}
+
+	/*
+	 *下载文件方式2
+	 **/
+	@RequestMapping("/downFile2")
+	public void downFile2(HttpServletResponse response) throws Exception {
+		File file = new File("D:/20171201_20180430订单信息查询.txt");
+		InputStream is = new FileInputStream(file);
+		OutputStream os = response.getOutputStream();
+		response.setContentType("application/x-download");
+		response.setHeader("Content-Disposition","attachment;filename="+URLEncoder.encode("我是谁.txt","utf-8"));
+		IoUtil.copy(is,os);
+	}
+
+	/*
+	* 根据日期创建不同目录
+	* */
+	public static void creatFile() throws Exception {
+		File file = new File("d:/data/2018-08-17");
+		if (!file.exists()){
+			file.mkdirs();
+		}
+		File file2 = new File(file.getPath(),"aa1.txt");
+		file2.createNewFile();
+	}
+
+	public static void main(String[] args) throws Exception {
+		File file = new File("D:/20171201_20180430订单信息查询.txt");
+		InputStream is = new FileInputStream(file);
+		List list = new ArrayList();
+		IoUtil.readUtf8Lines(is,list);
+		List<BBill> list2 = new ArrayList<>();
+
+		list.stream()
+			.forEach(bill -> {
+				BBill bBill = new BBill();
+				bBill = JSONUtil.toBean((String) bill,BBill.class);
+				list2.add(bBill);
+			});
+
+
+		PageInfo<BBill> pageInfo = null;
+		while(pageInfo == null || pageInfo.isHasNextPage()){
+			PageHelper.startPage(pageInfo == null ? 1 : pageInfo.getNextPage(),50);
+			pageInfo = new PageInfo<>(list2);
+			System.out.println(JSONUtil.toJsonStr(pageInfo) + "\n");
+		}
+
+	}
 
 }
